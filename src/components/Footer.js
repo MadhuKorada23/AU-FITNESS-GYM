401b4f8c0ae254f45accdf2925543821f62ee471
madhu <madhukorada23@gmail.com>
2025-01-13 22:44:55 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css'; // Import your CSS for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 AU GYM. All rights reserved.
+        Developed By Mahesh 
+      </p>
+      <a href='https://www.instagram.com/gymsankar_/?utm_source=ig_web_button_share_sheet'>Click here to follow  us on Instagram</a>
+    </footer>
+  );
+};
+
+export default Footer;
